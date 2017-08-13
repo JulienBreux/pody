f27a8723835157eeadbb9a0a623841d069f73ddf
Julien Breux <julien.breux@gmail.com>
2017-08-13 19:07:34 +0200

    Add key bindings. Close #15

---
+++ key.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/jroimartin/gocui"
+
+type Key struct {
+	viewname string
+	key      interface{}
+	handler  func(*gocui.Gui, *gocui.View) error
+}
+
+// Configure globale keys
+var globalKeys []Key = []Key{
+	Key{"", gocui.KeyCtrlC, quitAction},
+}
+
+// Define UI key bindings
+func uiKey(g *gocui.Gui) error {
+	// Glboal keys
+	for _, key := range globalKeys {
+		if err := g.SetKeybinding(key.viewname, key.key, gocui.ModNone, key.handler); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -6,28 +6,33 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
+// App
 func main() {
-	// Define UI
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
 	}
 	defer g.Close()
 
-	// Configure global UI
 	g.Highlight = true
 	g.SelFgColor = gocui.ColorGreen
 
-	// Define the layout
 	g.SetManagerFunc(uiLayout)
 
-	// Let's go dude!
+	if err := uiKey(g); err != nil {
+		log.Panicln(err)
+	}
+
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 }
 
-// Define the UI
 func uiLayout(g *gocui.Gui) error {
 	return nil
+}
+
+// Global action: Quit
+func quitAction(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
